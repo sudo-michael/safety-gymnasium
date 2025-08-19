539fe23f855700d52f216b4ae218dbfda94ac884
Michael Lu <lu.michael@outlook.com>
2025-08-18 17:16:43 -0700

    bump versiong

--- safety_gymnasium/version.py
+++ safety_gymnasium/version.py
@@ -14,9 +14,9 @@
 # ==============================================================================
 """Safety-Gymnasium."""
 
-__version__ = '1.2.0'
-__license__ = 'Apache License, Version 2.0'
-__author__ = 'Safety-Gymnasium Contributors'
+__version__ = "1.3.0"
+__license__ = "Apache License, Version 2.0"
+__author__ = "Safety-Gymnasium Contributors"
 __release__ = False
 
 if not __release__:
@@ -26,20 +26,20 @@ if not __release__:
     try:
         prefix, sep, suffix = (
             subprocess.check_output(  # noqa: S603,RUF100
-                ['git', 'describe', '--abbrev=7'],  # noqa: S607
+                ["git", "describe", "--abbrev=7"],  # noqa: S607
                 cwd=os.path.dirname(os.path.abspath(__file__)),
                 stderr=subprocess.DEVNULL,
                 text=True,
             )
             .strip()
-            .lstrip('v')
-            .replace('-', '.dev', 1)
-            .replace('-', '+', 1)
-            .partition('.dev')
+            .lstrip("v")
+            .replace("-", ".dev", 1)
+            .replace("-", "+", 1)
+            .partition(".dev")
         )
         if sep:
-            version_prefix, dot, version_tail = prefix.rpartition('.')
-            prefix = f'{version_prefix}{dot}{int(version_tail) + 1}'
+            version_prefix, dot, version_tail = prefix.rpartition(".")
+            prefix = f"{version_prefix}{dot}{int(version_tail) + 1}"
             __version__ = sep.join((prefix, suffix))
             del version_prefix, dot, version_tail
         else:
