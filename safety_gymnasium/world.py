d4a084627704849feb13b5afecb1e96fa8f9074f
Michael Lu <lu.michael@outlook.com>
2025-09-12 15:03:31 -0700

    remove breakpoint

--- safety_gymnasium/world.py
+++ safety_gymnasium/world.py
@@ -407,7 +407,6 @@ class World:  # pylint: disable=too-many-instance-attributes
         # Instantiate simulator
         # print(xmltodict.unparse(self.xml, pretty=True))
         self.xml_string = xmltodict.unparse(self.xml)
-        breakpoint()
         model = mujoco.MjModel.from_xml_string(self.xml_string)  # pylint: disable=no-member
         data = mujoco.MjData(model)  # pylint: disable=no-member
 
